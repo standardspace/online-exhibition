887389553b68ba09d8b8e91cd0c0c88748674b84
standardspace <patrick@standardspace.com>
2022-04-21 09:07:10 +0100

    Artist page tweaks

--- assets/js/build/script.js
+++ assets/js/build/script.js
@@ -1,24 +1,50 @@
 // Go wild...
 import Macy from "macy";
 
-const maGalleryGrid = Macy({
-  container: '.ma-gallery',
-  trueOrder: true,
-  waitForImages: false,
-  mobileFirst: true,
-  margin: 8,
-  columns: 1,
-  breakAt: {
-    520: {
-      margin: 16,
-      columns: 2
-    },
-    1024: {
-      margin: 32,
-      columns: 3
-    },
-    1440: {
-      columns: 4
+const grids = document.querySelectorAll('.ma-gallery');
+
+grids.forEach( grid => {
+  const maGalleryGrid = Macy({
+    container: grid,
+    trueOrder: true,
+    waitForImages: false,
+    mobileFirst: true,
+    margin: 8,
+    columns: 1,
+    breakAt: {
+      520: {
+        margin: 16,
+        columns: 2
+      },
+      1024: {
+        margin: 32,
+        columns: 3
+      },
+      1440: {
+        columns: 4
+      }
     }
-  }
-});
+  });
+})
+
+// const maGalleryGrid = Macy({
+//   container: '.ma-gallery',
+//   trueOrder: true,
+//   waitForImages: false,
+//   mobileFirst: true,
+//   margin: 8,
+//   columns: 1,
+//   breakAt: {
+//     520: {
+//       margin: 16,
+//       columns: 2
+//     },
+//     1024: {
+//       margin: 32,
+//       columns: 3
+//     },
+//     1440: {
+//       columns: 4
+//     }
+//   }
+// });
