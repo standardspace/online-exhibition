bb46a0f7cff87623986c5bd5039c7a6f40fff59b
standardspace <patrick@standardspace.com>
2022-04-23 11:33:28 +0100

    hardcode in header

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<?php wp_head(); ?>
+    <link rel="stylesheet" href="/wp-content/themes/online-exhibition/style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
