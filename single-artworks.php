6b2a690aea439bfbef1faa1b1cb37a1ebac91ac5
standardspace <patrick@standardspace.com>
2022-04-23 14:19:34 +0100

    Contain artworks on artwork page

--- single-artworks.php
+++ single-artworks.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<div class="ma-container">
+	<div class="ma-container ma-artwork-page">
     <?php
     if(have_posts()) : while(have_posts()) : the_post(); ?>
       <h1><?php the_title(); ?></h1>
