fc9eb64ff60c21599e526631d0d084374baee3dd
standardspace <patrick@standardspace.com>
2022-04-26 16:51:53 +0100

    Larger image

--- single-artworks.php
+++ single-artworks.php
@@ -3,7 +3,7 @@
     <?php
     if(have_posts()) : while(have_posts()) : the_post(); ?>
       <h1><?php the_title(); ?></h1>
-      <?php the_post_thumbnail($size); ?>
+      <?php the_post_thumbnail('full'); ?>
 
       <?php the_content(); ?> 
       <?php echo get_the_term_list( $post->ID, 'artists', 'Artist: ', ', ', '' ); ?> 
